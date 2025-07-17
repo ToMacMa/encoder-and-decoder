33d98e0764f58445ea123428d65bfcfb62485a87
ToMacMa <126681860+ToMacMa@users.noreply.github.com>
2025-07-17 21:08:57 +0200

    Additional functionality

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import os,json
 import urllib.request
-from createCode import *
+
+def downloadFileIfNone(url,path):
+    tmp = os.listdir()
+    if path not in tmp:
+        urllib.request.urlretrieve(url, filename=path)
+downloadFileIfNone("https://raw.githubusercontent.com/ToMacMa/encoder-and-decoder/refs/heads/main/createCode.py","createCode.py")
+try:
+    from createCode import *
+except:
+    print("Run this program again.")
+    quit()
 
 CHAR_LIST = []
 def encode():
@@ -10,11 +20,6 @@ def decode():
     pass
 
 files = os.listdir()
-
-def downloadFileIfNone(url,path):
-    tmp = os.listdir()
-    if path not in tmp:
-        urllib.request.urlretrieve(url, filename=path)
 
 downloadFileIfNone("https://raw.githubusercontent.com/ToMacMa/encoder-and-decoder/refs/heads/main/defaultCode.json",'defaultCode.json')
 files = os.listdir()
