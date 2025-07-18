f6a2fd95c47719bbebdf432b28d97e8bce001ed4
ToMacMa <126681860+ToMacMa@users.noreply.github.com>
2025-07-18 18:28:58 +0200

    r

--- createCode.py
+++ createCode.py
@@ -7,7 +7,14 @@ def createCode():
     for i in range(32,127):
         CHAR_LIST.append(chr(i))
         rat.append(chr(i))
+    CHAR_LIST.append('/n')
+    rat.append('/n')
     out.append(rat)
     shuffle(CHAR_LIST)
     out.append(CHAR_LIST)
     return out
+if __name__ == '__main__':
+    code = createCode()
+    f = open('defaultCode.json','w')
+    f.write(json.dumps(code,indent=1))
+    f.close()
