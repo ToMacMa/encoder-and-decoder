91560bc34e449b250ef9ec9154c126cc8442134c
ToMacMa <126681860+ToMacMa@users.noreply.github.com>
2025-07-18 18:40:52 +0200

    Gui can now encrypt and decrypt files!

--- gui.py
+++ gui.py
@@ -19,17 +19,17 @@ root.resizable(width=False,height=False)
 root.config(bg="white")
 root.title("GUI encryptor and decryptor.")
 
-root.rowconfigure([0,1,2],weight=1)
+root.rowconfigure([0,1,2,3],weight=1)
 root.columnconfigure([0,1,2],weight=1)
 
 
 tk.Label(root,text="To encrypt",font=("Arial",int(root.winfo_screenheight()/50)),bg="white").grid(column=0,row=0)
 tk.Label(root,text="To decrypt",font=("Arial",int(root.winfo_screenheight()/50)),bg="white").grid(column=2,row=0)
 
-hid1 = tk.Entry(root,text="",font=("Arial",int(root.winfo_screenheight()/120)),bg="white",state='readonly')
+hid1 = tk.Entry(root,text="",font=("Arial",int(root.winfo_screenheight()/75)),bg="white",state='readonly')
 hid1.grid(column=0,row=2)
 
-hid2 = tk.Entry(root,text="",font=("Arial",int(root.winfo_screenheight()/120)),bg="white",state='readonly')
+hid2 = tk.Entry(root,text="",font=("Arial",int(root.winfo_screenheight()/75)),bg="white",state='readonly')
 hid2.grid(column=2,row=2)
 
 def run(event=None):
@@ -41,6 +41,31 @@ def run(event=None):
     hid2.delete(0,tk.END)
     hid2.insert(0,str(decode(code,str(input2.get()))))
     hid2['state'] = 'readonly'
+
+def run2(event=None):
+    try:
+        encodeFile(code,str(input1.get()))
+        hid1['state'] = 'normal'
+        hid1.delete(0,tk.END)
+        hid1.insert(0,"Done!")
+        hid1['state'] = 'readonly'
+    except:
+        hid1['state'] = 'normal'
+        hid1.delete(0,tk.END)
+        hid1.insert(0,"An error has occured.")
+        hid1['state'] = 'readonly'
+    try:
+        decodeFile(code,str(input2.get()))
+        hid2['state'] = 'normal'
+        hid2.delete(0,tk.END)
+        hid2.insert(0,"Done!")
+        hid2['state'] = 'readonly'
+    except:
+        hid2['state'] = 'normal'
+        hid2.delete(0,tk.END)
+        hid2.insert(0,"An error has occured.")
+        hid2['state'] = 'readonly'
+
 input1 = tk.Entry(root,font=("Arial",int(root.winfo_screenheight()/75)),bg="white")
 input1.grid(column=0,row=1)
 
@@ -48,7 +73,10 @@ input2 = tk.Entry(root,font=("Arial",int(root.winfo_screenheight()/75)),bg="whit
 input2.grid(column=2,row=1)
 
 button1 = tk.Button(root,font=("Arial",int(root.winfo_screenwidth()/100)),text="Encrypt and decrypt",bg="white",command=lambda:run())
-button1.grid(column=1,row=2)
+button1.grid(column=0,row=3)
+
+button2 = tk.Button(root,font=("Arial",int(root.winfo_screenwidth()/100)),text="File encrypt and decrypt",bg="white",command=lambda:run2())
+button2.grid(column=2,row=3)
 
 button1.bind('<Key>',run)
 root.mainloop()
